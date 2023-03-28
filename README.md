f2957765805f1ffe8b66b1fcb9db385615a2aaab
SyimykMitalipove <syimyk310703@gmail.com>
2023-03-28 17:38:04 +0600

    build githubpapi project

---
+++ README.md
@@ -0,0 +1,4 @@
+# GITHUB API Project.
+
+
+## Create project using React, TypeScript, GitHubAPI, for styles using: SCSS , custom creating components
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "SyimykMitalipov",
+  "avatar": "https://avatars.githubusercontent.com/u/75716496?v=4",
+  "name": "Syimyk",
+  "company": "GeekStudio",
+  "blog": "https://syimykmitalipov.github.io/digital-cv/",
+  "location": "Bishkek",
+  "bio": "Front-End Developer",
+  "twitter": "@SyimykMitalipov",
+  "repos": 12,
+  "followers": 9,
+  "following": 7,
+  "created": "2020-12-09T05:43:00Z",
+}
